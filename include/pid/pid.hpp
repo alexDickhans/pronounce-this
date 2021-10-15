76e1d9104f0ae079fa6b0d0ad5c84adb406b43fe
Alex <adickhans@gmail.com>
2021-10-15 10:40:15 -0600

    Add getters and setters to PID class

--- include/pid/pid.hpp
+++ include/pid/pid.hpp
@@ -4,6 +4,10 @@
 #include "utils/utils.hpp"
 
 namespace Pronounce {
+
+
+    double signum_c(double x);
+
     class PID {
     private:
         double kP;
@@ -26,6 +30,46 @@ namespace Pronounce {
         double power;
     public:
         PID(double kP, double kI, double kD, double target, double position);
+
+        double getPower() {
+            return power;
+        }
+
+        void setPower(double power) {
+            this->power = power;
+        }
+
+        double getTarget() {
+            return target;
+        }
+
+        void setTarget(double target) {
+            this->target = target;
+        }
+        
+        double getKP() {
+            return kP;
+        }
+
+        void setKP(double kP) {
+            this->kP = kP;
+        }
+
+        double getKI() {
+            return kI;
+        }
+
+        void setKI(double kI) {
+            this->kI = kI;
+        }
+
+        double getKD() {
+            return kD;
+        }
+
+        void setKD(double kD) {
+            this->kD = kD;
+        }
 
         double update();
 
