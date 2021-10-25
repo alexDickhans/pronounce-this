4ed86b0ce1a79679dda8f40dfefa4b2ac6a16c32
Alex <adickhans@gmail.com>
2021-10-25 17:39:41 -0600

    Fix minor build error

--- src/position/trackingWheel.cpp
+++ src/position/trackingWheel.cpp
@@ -8,10 +8,6 @@ namespace Pronounce {
         this->rotationSensor = rotationSensor;
     }
 
-    void TrackingWheel::update() {
-        this->setPosition(rotationSensor->get_position() * this->getRadius() * M_PI * 2.0 * this->getTuningFactor());
-    }
-
     TrackingWheel::~TrackingWheel() {
     }
 } // namespace Pronounce
