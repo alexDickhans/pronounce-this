b3ebb951f0e8832f0fc1d540acb3bd090db6d42c
Alex <adickhans@gmail.com>
2021-11-08 13:00:27 -0700

    Add vector helper functions

--- include/utils/vector.hpp
+++ include/utils/vector.hpp
@@ -6,6 +6,7 @@ namespace Pronounce {
     /**
      * @brief A class to handle vectors
      * 
+     * @author Alex(ad101-lab)
      */
     class Vector {
     private:
@@ -14,36 +15,79 @@ namespace Pronounce {
     public:
         Vector();
         Vector(double magnitude, double angle);
+        Vector(Point point);
 
+        /**
+         * @brief Get the vector in the cartesian reference frame
+         * 
+         * @return Point 
+         */
         Point getCartesian();
 
         // Vector Math
         
+        /**
+         * @brief Get the dot product of this vector and x
+         * 
+         * @param x Other vector
+         * @return double The dot product of this vector and another vector
+         */
         double dot(Vector x);
+
+        /**
+         * @brief Get the sum of this vector and x
+         * 
+         * @param x 
+         * @return Vector 
+         */
         Vector addition(Vector x);
+
+        /**
+         * @brief Scale this vector by a scalar
+         * 
+         * @param scalar The scalar double
+         * @return Vector The resulting vector
+         */
         Vector scale(double scalar);
+        
+        /**
+         * @brief Get the Magnitude 
+         * 
+         * @return double Magnitude
+         */
+        double getMagnitude() {
+            return magnitude;
+        }
+
+        /**
+         * @brief Set the magnitude
+         * 
+         * @param magnitude New magnitude value
+         */
+        void setMagnitude(double magnitude) {
+            this->magnitude = magnitude;
+        }
+
+        /**
+         * @brief Get the angle 
+         * 
+         * @return double angle
+         */
+        double getAngle() {
+            return angle;
+        }
+
+        /**
+         * @brief Set the angle
+         * 
+         * @param magnitude New angle value
+         */
+        void setAngle(double angle) {
+            this->angle = angle;
+        }
 
         ~Vector();
     };
-
-    Vector::Vector() {
-        this->magnitude = 0;
-        this->angle = 0;
-    }
-
-    Vector::Vector(double magnitude, double angle) {
-        this->magnitude = magnitude;
-        this->angle = angle;
-    }
-
-    Point Vector::getCartesian() {
-        Point result = Point();
-        result.setX(magnitude * cos(angle));
-        return result;
-    }
-
-    Vector::~Vector() {
-    }
 } // namespace Pronounce
 
 
---
+++ src/utils/vector.cpp
@@ -0,0 +1,51 @@
+#include "vector.hpp"
+
+namespace Pronounce {
+    Vector::Vector() {
+        this->magnitude = 0;
+        this->angle = 0;
+    }
+
+    Vector::Vector(double magnitude, double angle) {
+        this->magnitude = magnitude;
+        this->angle = angle;
+    }
+
+    Vector::Vector(Point point) {
+        this->magnitude = point.distance(Point(0, 0));
+        this->angle = acos(point.getX() / magnitude);
+    }
+
+    Point Vector::getCartesian() {
+        Point result = Point();
+        result.setX(magnitude * cos(angle));
+        return result;
+    }
+
+    double Vector::dot(Vector x) {
+        Point thisPoint = this->getCartesian();
+        Point xPoint = this->getCartesian();
+
+        double result = thisPoint.getX() * xPoint.getX() +
+            thisPoint.getY() * xPoint.getY();
+    }
+
+    Vector Vector::addition(Vector x) {
+        Point thisPoint = this->getCartesian();
+        Point xPoint = this->getCartesian();
+
+        Point resultPoint = Point(thisPoint.getX() + xPoint.getX(), thisPoint.getY() + xPoint.getY());
+        Vector result = Vector(resultPoint);
+    }
+
+    Vector Vector::scale(double scalar) {
+        Vector vector = Vector(this);
+
+        vector.setMagnitude(vector.getMagnitude() * scalar);
+
+        return vector;
+    }
+
+    Vector::~Vector() {
+    }
+} // namespace Pronounce
