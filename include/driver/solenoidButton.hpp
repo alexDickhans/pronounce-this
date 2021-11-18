b0d12150e5bfd51c401b45e1770e5f664fc9b7c6
Alex Dickhans <55064247+ad101-lab@users.noreply.github.com>
2021-11-18 09:02:47 -0700

    Add solenoid button

---
+++ include/driver/solenoidButton.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "api.h"
+#include "button.h"
+
+namespace Pronounce {
+    class SolenoidButton : public Button {
+    private:
+        pros::ADIDigitalOut solenoid;
+
+        bool inverted{false};
+        bool retainOnNeutral{true};
+    public:
+        SolenoidButton(pros::Controller* controller);
+        SolenoidButton(pros::Controller* controller, pros::ADIDigitalOut* solenoid);
+        SolenoidButton(pros::Controller* controller, pros::controller_digital_e_t positiveButton, pros::controller_digital_e_t negativeButton);
+
+        void updateActuator();
+
+        bool getInverted() {
+            return inverted;
+        }
+
+        void setInverted(bool inverted) {
+            this->inverted = inverted;
+        }
+
+        pros::ADIDigitalOut getSolenoid() {
+            return solenoid;
+        }
+
+        void setSolenoid(pros::ADIDigitalOut solenoid) {
+            this->solenoid = solenoid;
+        }
+    }
+} // Pronounce
---
+++ src/driver/solenoidButton.cpp
@@ -0,0 +1,32 @@
+#include "solenoidButton.hpp"
+
+namespace Pronounce {
+    SolenoidButton::SolenoidButton(pros::Controller* controller) : Button(controller) {
+
+    }
+    SolenoidButton::SolenoidButton(pros::Controller* controller, pros::ADIDigitalOut* solenoid) : Button(controller) {
+        this->solenoid = solenoid
+    }
+    SolenoidButton::SolenoidButton(pros::Controller* controller, pros::controller_digital_e_t positiveButton, pros::controller_digital_e_t negativeButton) : Button(controller, positiveButton, negativeButton) {
+        
+    }
+
+    SolenoidButton::updateActuator() {
+        switch (this->getButtonStatus()) {
+            case ButtonStatus::POSITIVE:
+                this->solenoid.set_value(!this->inverted);
+                break;
+            case ButtonStatus::NEGATIVE:
+                this->solenoid.set_value(this->inverted);
+                break;
+            case ButtonStatus::NEUTRAL:
+            default:
+                if (retainOnNeutral) {
+                    // Nothing
+                } else {
+                    this->solenoid.set_value(this->inverted);
+                }
+                break;
+        }
+    }
+} // Pronounce
