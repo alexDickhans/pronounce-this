5d80fb0fff019048651b791fe57562d1ef8c90fc
Alex <adickhans@gmail.com>
2021-11-23 10:24:32 -0700

    Remove unneccecary if statement

--- src/odometry/threeWheelOdom.cpp
+++ src/odometry/threeWheelOdom.cpp
@@ -27,20 +27,14 @@ namespace Pronounce {
         double averageOrientation = lastAngle + (angleChange / 2);
 
         Vector localOffset;
-        if (angleChange == 0) {
-            localOffset = Vector(new Point(deltaBack, deltaRight));
-        }
-        else {
-            double PI_2 = 2 * M_PI;
-            localOffset = Vector(new Point(deltaBack + ((angleChange/PI_2) * PI_2 * backOffset), deltaRight + ((angleChange/PI_2) * PI_2 * rightOffset)));
-        }
-        //std::cout << localOffset.getCartesian().getX() - deltaBack << std::endl;
+        double PI_2 = 2 * M_PI;
+        localOffset = Vector(new Point(deltaBack + ((angleChange / PI_2) * PI_2 * backOffset), deltaRight + ((angleChange / PI_2) * PI_2 * rightOffset)));
 
         // Rotate vector
         localOffset.setAngle(localOffset.getAngle() - averageOrientation);
 
         lastPosition->add(localOffset.getCartesian());
-        lastPosition->setTheta(fmod(currentAngle+M_PI * 2, M_PI * 2));
+        lastPosition->setTheta(fmod(currentAngle + M_PI * 2, M_PI * 2));
 
         this->setPosition(lastPosition);
     }
