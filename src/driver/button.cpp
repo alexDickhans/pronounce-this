0a6dda74d81a8f1ba438ff5e032183f048a3c110
Alex <adickhans@gmail.com>
2021-12-03 12:45:40 -0700

    Add toggle button config

--- src/driver/button.cpp
+++ src/driver/button.cpp
@@ -1,7 +1,7 @@
 #include "button.hpp"
 
 namespace Pronounce {
-    Button::Button(pros::Controller* controller){
+    Button::Button(pros::Controller* controller) {
         this->controller = controller;
         this->positiveButton = pros::E_CONTROLLER_DIGITAL_L1;
         this->negativeButton = pros::E_CONTROLLER_DIGITAL_L2;
@@ -12,18 +12,30 @@ namespace Pronounce {
         this->positiveButton = positiveButton;
         this->negativeButton = negativeButton;
     }
-    
+
     void Button::update() {
         updateController();
         updateActuator();
     }
 
     void Button::updateController() {
+        // Update the controller if the controller toggles with a button
+        if (singleToggle) {
+            // Toggle button status between neutral and positive
+            if (controller->get_digital_new_press(positiveButton)) {
+                buttonStatus = buttonStatus == ButtonStatus::POSITIVE ? ButtonStatus::NEUTRAL : ButtonStatus::POSITIVE;
+                return;
+            }
+        }
+
+        // Update button status
         if (controller->get_digital(positiveButton)) {
             buttonStatus = ButtonStatus::POSITIVE;
-        } else if (controller->get_digital(negativeButton)) {
+        }
+        else if (controller->get_digital(negativeButton)) {
             buttonStatus = ButtonStatus::NEGATIVE;
-        } else {
+        }
+        else {
             buttonStatus = ButtonStatus::NEUTRAL;
         }
     }
