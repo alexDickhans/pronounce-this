f34dc75c5b9b6b6f55af074ef9a3c42b41467194
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2022-01-18 19:13:42 -0700

    Add height adjustment implementation

--- src/driver/motorButton.cpp
+++ src/driver/motorButton.cpp
@@ -3,72 +3,83 @@
 
 
 namespace Pronounce {
-    MotorButton::MotorButton(pros::Controller* controller, pros::Motor* motor,
-            pros::controller_digital_e_t positiveButton,
-            pros::controller_digital_e_t negativeButton,
-            int positiveAuthority,
-            int neutralAuthority,
-            int negativeAuthority,
-            int min,
-            int max) : Button(controller, positiveButton, negativeButton) {
-        this->motor = motor;
-        this->positiveAuthority = positiveAuthority;
-        this->neutralAuthority = neutralAuthority;
-        this->negativeAuthority = negativeAuthority;
-        this->min = min;
-        this->max = max;
-    }
+	MotorButton::MotorButton(pros::Controller* controller, pros::Motor* motor,
+		pros::controller_digital_e_t positiveButton,
+		pros::controller_digital_e_t negativeButton,
+		int positiveAuthority,
+		int neutralAuthority,
+		int negativeAuthority,
+		int min,
+		int max) : Button(controller, positiveButton, negativeButton) {
+		this->motor = motor;
+		this->positiveAuthority = positiveAuthority;
+		this->neutralAuthority = neutralAuthority;
+		this->negativeAuthority = negativeAuthority;
+		this->min = min;
+		this->max = max;
+	}
 
-    void MotorButton::updateActuator() {
-        if (!this->getEnabled()) {
-            this->motor->move_velocity(0.0);
-            return;
-        }
+	void MotorButton::updateActuator() {
+		if (!this->getEnabled()) {
+			this->motor->move_velocity(0.0);
+			return;
+		}
 
-        if (this->getAutonomous() && !this->autonomousButton) {
-            this->motor->move_absolute(this->autonomousAuthority, abs(this->positiveAuthority));
-            return;
-        }
+		if (this->getAutonomous() && !this->autonomousButton) {
+			if (this->goToHeight) {
+				// Use autonomous authority to calculate the angle that the lift needs to be at. 
+				double angle = asin((this->autonomousAuthority - this->height) / this->length) * this->gearRatio;
 
-        switch (this->getButtonStatus()) {
-        case NEGATIVE:
-            if (goToImmediately) {
-                this->motor->move_absolute(min, negativeAuthority);
-            }
-            else if (this->motor->get_position() > this->min && this->min < this->max) {
-                this->motor->move(negativeAuthority);
-            } else if (this->min >= this->max) {
-                this->motor->move(negativeAuthority);
-            }
-            else {
-                this->motor->move_velocity(neutralAuthority);
-            }
-            break;
-        case POSITIVE:
-            if (goToImmediately) {
-                this->motor->move_absolute(max, positiveAuthority);
-            }
-            else if (this->motor->get_position() < this->max && this->min < this->max) {
-                this->motor->move(positiveAuthority);
-            }else if (this->min >= this->max) {
-                this->motor->move(positiveAuthority);
-            }
-            else {
-                this->motor->move_velocity(neutralAuthority);
-            }
-            break;
-        case NEUTRAL:
-        default:
-            if (this->getSingleToggle() && goToImmediately) {
-                this->motor->move_absolute(min, neutralAuthority);
-            }
-            if (goToImmediately)
-                return;
-            this->motor->move_velocity(neutralAuthority);
-            break;
-        }
-    }
+				// Set the lift to the angle.
+				this->motor->move_absolute(angle, abs(this->positiveAuthority));
+			}
+			else {
+				this->motor->move_absolute(this->autonomousAuthority, abs(this->positiveAuthority));
+			}
+			return;
+		}
 
-    MotorButton::~MotorButton() {
-    }
+		switch (this->getButtonStatus()) {
+		case NEGATIVE:
+			if (goToImmediately) {
+				this->motor->move_absolute(min, negativeAuthority);
+			}
+			else if (this->motor->get_position() > this->min && this->min < this->max) {
+				this->motor->move(negativeAuthority);
+			}
+			else if (this->min >= this->max) {
+				this->motor->move(negativeAuthority);
+			}
+			else {
+				this->motor->move_velocity(neutralAuthority);
+			}
+			break;
+		case POSITIVE:
+			if (goToImmediately) {
+				this->motor->move_absolute(max, positiveAuthority);
+			}
+			else if (this->motor->get_position() < this->max && this->min < this->max) {
+				this->motor->move(positiveAuthority);
+			}
+			else if (this->min >= this->max) {
+				this->motor->move(positiveAuthority);
+			}
+			else {
+				this->motor->move_velocity(neutralAuthority);
+			}
+			break;
+		case NEUTRAL:
+		default:
+			if (this->getSingleToggle() && goToImmediately) {
+				this->motor->move_absolute(min, neutralAuthority);
+			}
+			if (goToImmediately)
+				return;
+			this->motor->move_velocity(neutralAuthority);
+			break;
+		}
+	}
+
+	MotorButton::~MotorButton() {
+	}
 } // namespace Pronounce
