120c363e3cb8c5074e7eb3ad890d2ef7aad4ef9b
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2022-05-19 17:47:36 -0600

    Working on sequences

--- include/stateMachine/sequence.hpp
+++ include/stateMachine/sequence.hpp
@@ -1,14 +1,47 @@
 #pragma once
 
-#include <vector>
 #include "behavior.hpp"
+#include "stateController.hpp"
+#include <vector>
 
 namespace Pronounce {
 	class Sequence : public Behavior {
 	private:
-		
+		std::vector<StateController*> stateControllers;
+		std::vector<Behavior*> behaviors;
+		int currentIndex = 0;
 	public:
-		Sequence(/* args */);
+		Sequence();
+
+		void initialize() {
+			if (behaviors.size() > 0) {
+				stateControllers.at(0)->initialize();
+			}
+		}
+
+		void update() {
+			if (stateControllers.at(currentIndex)->isDone()) {
+				if (currentIndex < stateControllers.size() - 1) {
+					stateControllers.at(currentIndex)->useDefaultBehavior();
+					currentIndex++;
+					stateControllers.at(currentIndex)->setCurrentBehavior(behaviors.at(currentIndex));
+				}
+				else {
+					stateControllers.at(currentIndex)->useDefaultBehavior();
+					// Done
+				}
+			}
+		}
+
+		void exit() {
+			stateControllers.at(currentIndex)->useDefaultBehavior();
+			currentIndex = stateControllers.size()-1;
+		}
+
+		bool isDone() {
+			return currentIndex >= stateControllers.size()-1;
+		}
+
 		~Sequence();
 	};
 } // namespace Pronounce
