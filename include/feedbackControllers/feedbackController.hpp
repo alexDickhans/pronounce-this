bed7623e86418e828e3edd0e6006e93823167234
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2022-08-16 10:04:32 -0600

    add docstrings to ir beambreak

--- include/feedbackControllers/feedbackController.hpp
+++ include/feedbackControllers/feedbackController.hpp
@@ -3,44 +3,117 @@
 // TODO: Add comments
 
 namespace Pronounce {
+	/**
+	 * @brief Feedback controller parent class
+	 * 
+	 * @authors Alex Dickhans (alexDickhans) 
+	 */
 	class FeedbackController {
 	private:
 	protected:
+		/**
+		 * @brief Target position of the feedback controller
+		 * 
+		 */
 		double target = 0.0;
+
+		/**
+		 * @brief The current position of the feedback controller
+		 * 
+		 */
 		double position = 0.0;
+
+		/**
+		 * @brief The output power
+		 * 
+		 */
 		double power;
+
+		/**
+		 * @brief The maximum power
+		 * 
+		 */
 		double maxPower = 1.0;
 	public:
+		/**
+		 * @brief Default constructor
+		 * 
+		 */
 		FeedbackController(/* args */) {}
-
+		
+		/**
+		 * @brief Update the feedback controller and 
+		 * 
+		 * @param input The sensor value from this loop
+		 * @return double The output of the loop
+		 */
 		virtual double update(double input) { return 0; }
 
+		/**
+		 * @brief Reset all the values
+		 * 
+		 */
 		virtual void reset() {}
 
+		/**
+		 * @brief Set the Target value
+		 * 
+		 * @param target New target value
+		 */
 		void setTarget(double target) {
 			this->target = target;
 		}
 
+		/**
+		 * @brief Get the Target value
+		 * 
+		 * @return double Target value
+		 */
 		double getTarget() {
 			return target;
 		}
 
+		/**
+		 * @brief Set the Position/input value
+		 * 
+		 * @param target The new position value
+		 */
 		void setPosition(double target) {
 			this->target = target;
 		}
 
+		/**
+		 * @brief Get the Position value
+		 * 
+		 * @return double The position value
+		 */
 		double getPosition() {
 			return position;
 		}
 
+		/**
+		 * @brief Get the Power value
+		 * 
+		 * @return double The power from the last loop
+		 */
 		double getPower() {
 			return power;
 		}
 
+		/**
+		 * @brief Set the Max Power value
+		 * 
+		 * @param maxPower New max power
+		 */
 		void setMaxPower(double maxPower) {
 			this->maxPower = maxPower;
 		}
 
+		/**
+		 * @brief Get the Max Power object
+		 * 
+		 * @return double The current max power
+		 */
 		double getMaxPower() {
 			return maxPower;
 		}
--- include/sensors/IRBeambreak.hpp
+++ include/sensors/IRBeambreak.hpp
@@ -3,27 +3,61 @@
 #include "api.h"
 
 // TODO: Test code
-// TODO: add docstrings
-// TODO: Comments
 
 namespace Pronounce {
+
+	/**
+	 * @brief IR beambreak class for ir beambreaks
+	 * 
+	 * @authors Alex Dickhans (alexDickhans) 
+	 */
 	class IRBeambreak {
 	private:
+		/**
+		 * @brief The analog sensor
+		 * 
+		 */
 		pros::ADIAnalogIn* irSensor;
-		int threshold;
+
+		/**
+		 * @brief Minimum threshold for a detection
+		 * 
+		 */
+		int32_t threshold;
 	public:
+		/**
+		 * @brief Construct a new IRBeambreak object
+		 * 
+		 * @param irSensor 
+		 */
 		IRBeambreak(pros::ADIAnalogIn* irSensor) {
 			this->irSensor = irSensor;
 		}
 
+		/**
+		 * @brief Get the Value from the sensor, thresholded
+		 * 
+		 * @return true The value is greater than the threshold
+		 * @return false The value is less than the threshold
+		 */
 		bool getValue() {
-			return irSensor->get_value() < threshold;
+			return irSensor->get_value() >= threshold;
 		}
 
+		/**
+		 * @brief Get the a pointer to the sensor
+		 * 
+		 * @return pros::ADIAnalogIn* Pointer to the sensor
+		 */
 		pros::ADIAnalogIn* getIrSensor() {
 			return irSensor;
 		}
 
+		/**
+		 * @brief Set the a pointer to the sensor
+		 * 
+		 * @param irSensor Pointer to the sensor object 
+		 */
 		void setIrSensor(pros::ADIAnalogIn* irSensor) {
 			this->irSensor = irSensor;
 		}
