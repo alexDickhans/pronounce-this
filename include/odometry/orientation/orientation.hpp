f3830f69f6d9af409a36d4421020d8d4c62127fe
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2022-08-16 17:09:35 -0600

    Orientation.hpp class

--- include/odometry/orientation/orientation.hpp
+++ include/odometry/orientation/orientation.hpp
@@ -2,26 +2,50 @@
 
 #include "units/units.hpp"
 
-// TODO: Add docstrings
-// TODO: add comments
-
 namespace Pronounce {
+
+	/**
+	 * @brief Orientation class that keeps track of different types of orientation objects
+	 * 
+	 */
 	class Orientation {
 	private:
+		/**
+		 * @brief The angle of the object
+		 * 
+		 */
 		Angle angle;
 	public:
 		Orientation(Angle angle) : angle(angle) { }
 
+		/**
+		 * @brief Update the values
+		 * 
+		 */
 		virtual void update() {}
 
+		/**
+		 * @brief Set the Angle object
+		 * 
+		 * @param angle New angle
+		 */
 		void setAngle(Angle angle) {
 			this->angle = angle;
 		}
 
+		/**
+		 * @brief Get the Angle object
+		 * 
+		 * @return Angle The new angle
+		 */
 		Angle getAngle() {
 			return angle;
 		}
 
+		/**
+		 * @brief Reset the values
+		 * 
+		 */
 		virtual void reset() {
 			angle = 0.0;
 		}
