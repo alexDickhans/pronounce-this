f76a9eb5f4b7db5daedd8a18ce3024aa5265b67e
ad101-lab <55064247+alexDickhans@users.noreply.github.com>
2022-10-05 19:51:55 -0600

    Add outline for tank motion profiling

--- include/motionControl/tankMotionProfiling.hpp
+++ include/motionControl/tankMotionProfiling.hpp
@@ -1,19 +1,30 @@
 #pragma once
 
+#include "velocityProfile/velocityProfile.hpp"
+#include "velocityProfile/sinusoidalVelocityProfile.hpp"
+#include "chassis/abstractTankDrivetrain.hpp"
+#include "stateMachine/behavior.hpp"
+#include "api.h"
+#include "odometry/continuousOdometry/continuousOdometry.hpp"
+
 namespace Pronounce {
-	class TankMotionProfiling {
+	class TankMotionProfiling : public Behavior {
 	private:
-		
+		pros::Mutex& drivetrainMutex;
+
+		AbstractTankDrivetrain& drivetrain;
+		VelocityProfile velocityProfile;
+
+		ContinuousOdometry& odometry;
 	public:
-		TankMotionProfiling(/* args */);
-		~TankMotionProfiling();
+		TankMotionProfiling(std::string name, AbstractTankDrivetrain& drivetrain, VelocityProfile velocityProfile, ContinuousOdometry& odometry, pros::Mutex& drivetrainMutex) : Behavior(name), drivetrain(drivetrain), velocityProfile(velocityProfile), odometry(odometry), drivetrainMutex(drivetrainMutex) {
+
+		}
+
+		TankMotionProfiling(std::string name, AbstractTankDrivetrain& drivetrain, ProfileConstraints profileConstraints, QLength distance, ContinuousOdometry& odometry, pros::Mutex& drivetrainMutex) : Behavior(name), drivetrain(drivetrain), odometry(odometry), drivetrainMutex(drivetrainMutex) {
+			velocityProfile = SinusoidalVelocityProfile(distance, profileConstraints);
+		}
+
+		~TankMotionProfiling() {}
 	};
-	
-	TankMotionProfiling::TankMotionProfiling(/* args */)
-	{
-	}
-	
-	TankMotionProfiling::~TankMotionProfiling()
-	{
-	}
 } // namespace Pronounce
