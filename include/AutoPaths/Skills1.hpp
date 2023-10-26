d8fd20a865ed0f855fde212521239f2e58184882
alexDickhans <55064247+alexDickhans@users.noreply.github.com>
2023-10-26 14:25:51 -0600

    Planner has other features now

--- include/AutoPaths/Skills1.hpp
+++ include/AutoPaths/Skills1.hpp
@@ -4,7 +4,7 @@ std::vector<std::pair<PathPlanner::BezierSegment, Pronounce::SinusoidalVelocityP
 PathPlanner::Point(11.4689_in, 26.1017_in),
 PathPlanner::Point(11.6667_in, 17.5989_in),
 PathPlanner::Point(15.0282_in, 14.0395_in),
-PathPlanner::Point(22.1469_in, 8.10734_in)
+PathPlanner::Point(21.9492_in, 7.51412_in)
 ,false),
 nullptr},
 };
