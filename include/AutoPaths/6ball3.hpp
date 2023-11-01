f6922e67bb76ecfb06070c039bcc1a29e4947d24
alexDickhans <55064247+alexDickhans@users.noreply.github.com>
2023-11-01 17:11:36 -0600

    6 ball working woop woop

--- include/AutoPaths/6ball3.hpp
+++ include/AutoPaths/6ball3.hpp
@@ -3,21 +3,21 @@
 std::vector<std::pair<PathPlanner::BezierSegment, Pronounce::SinusoidalVelocityProfile*>> Auto6BallPath3 = {{PathPlanner::BezierSegment(
 PathPlanner::Point(93_in, 90_in),
 PathPlanner::Point(93_in, 100_in),
-PathPlanner::Point(80.2825_in, 98.2768_in),
-PathPlanner::Point(81.6667_in, 115.48_in)
+PathPlanner::Point(78.8983_in, 99.0678_in),
+PathPlanner::Point(81.2712_in, 114.096_in)
 ,false),
 nullptr},
 {PathPlanner::BezierSegment(
 PathPlanner::Point(81.6667_in, 115.48_in),
 PathPlanner::Point(84.435_in, 106.384_in),
-PathPlanner::Point(101.639_in, 97.4856_in),
-PathPlanner::Point(111.327_in, 95.5084_in)
+PathPlanner::Point(101.639_in, 95.5082_in),
+PathPlanner::Point(111.327_in, 93.531_in)
 ,true),
 nullptr},
 {PathPlanner::BezierSegment(
-PathPlanner::Point(107.571_in, 96.1018_in),
-PathPlanner::Point(100.451_in, 91.7513_in),
-PathPlanner::Point(91.9492_in, 82.8529_in),
+PathPlanner::Point(107.373_in, 93.7289_in),
+PathPlanner::Point(99.2651_in, 90.5648_in),
+PathPlanner::Point(91.9492_in, 85.2258_in),
 PathPlanner::Point(91.9491_in, 79.2937_in)
 ,false),
 nullptr},
