96490ee1a80fd7dfc3832ded8d013508444fe4bb
alexDickhans <55064247+alexDickhans@users.noreply.github.com>
2024-01-13 22:07:28 -0700

    Remove unneccesary files

--- include/driver.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "api.h"
-
-namespace Pronounce {
-#define LAUNCH_BUTTON pros::E_CONTROLLER_DIGITAL_R1
-#define INTAKE_BUTTON pros::E_CONTROLLER_DIGITAL_R2
-} // namespace Pronounce
