5900735f7bb8a46679af06eaf75d00fae706fda4
Alex Dickhans <55064247+alexDickhans@users.noreply.github.com>
2024-03-05 13:07:37 -0700

    Restore main.h

--- include/main.h
+++ include/main.h
@@ -1,63 +1,86 @@
-/**
- * \file main.h
- *
- * Contains common definitions and header files used throughout your PROS
- * project.
- *
- * \copyright Copyright (c) 2017-2023, Purdue University ACM SIGBots.
- * All rights reserved.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- */
-
 #ifndef _PROS_MAIN_H_
 #define _PROS_MAIN_H_
 
-/**
- * If defined, some commonly used enums will have preprocessor macros which give
- * a shorter, more convenient naming pattern. If this isn't desired, simply
- * comment the following line out.
- *
- * For instance, E_CONTROLLER_MASTER has a shorter name: CONTROLLER_MASTER.
- * E_CONTROLLER_MASTER is pedantically correct within the PROS styleguide, but
- * not convienent for most student programmers.
- */
-#define PROS_USE_SIMPLE_NAMES
-
-/**
- * If defined, C++ literals will be available for use. All literals are in the
- * pros::literals namespace.
- *
- * For instance, you can do `4_mtr = 50` to set motor 4's target velocity to 50
- */
-#define PROS_USE_LITERALS
+//#define PROS_USE_LITERALS
 
 #include "api.h"
+
 
 /**
  * You should add more #includes here
  */
-//#include "okapi/api.hpp"
+//#include "pros/api_legacy.h"
 
-/**
- * If you find doing pros::Motor() to be tedious and you'd prefer just to do
- * Motor, you can use the namespace with the following commented out line.
- *
- * IMPORTANT: Only the okapi or pros namespace may be used, not both
- * concurrently! The okapi namespace will export all symbols inside the pros
- * namespace.
- */
-// using namespace pros;
-// using namespace pros::literals;
-// using namespace okapi;
+// 2654lib includes
 
-/**
- * Prototypes for the competition control tasks are redefined here to ensure
- * that they can be called from user code (i.e. calling autonomous from a
- * button press in opcontrol() for testing purposes).
- */
+#include "pathPlanner/pathPlanner.hpp"
+
+#include "auton.h"
+
+// Chassis
+#include "chassis/abstractDrivetrain.hpp"
+#include "chassis/hardwareDrivetrain.hpp"
+#include "chassis/tankDrive.hpp"
+
+// FeedbackControllers
+#include "feedbackControllers/bangBang.hpp"
+#include "feedbackControllers/pid.hpp"
+#include "feedbackControllers/feedbackController.hpp"
+#include "feedbackControllers/flywheelPID.hpp"
+
+// Motion control
+#include "motionControl/rotationController.hpp"
+#include "motionControl/tankMotionProfiling.hpp"
+
+// Orientation
+#include "odometry/orientation/avgOrientation.hpp"
+#include "odometry/orientation/imu.hpp"
+#include "odometry/orientation/orientation.hpp"
+
+// Continuous Odometry
+#include "odometry/continuousOdometry/continuousOdometry.hpp"
+#include "odometry/continuousOdometry/threeWheelOdom.hpp"
+#include "odometry/continuousOdometry/particleFilterOdometry.hpp"
+
+// Interrupt Odom
+#include "odometry/interruptOdometry/gpsOdometry.hpp"
+#include "odometry/interruptOdometry/interruptOdometry.hpp"
+
+#include "pathPlanner/pathPlanner.hpp"
+
+// Position
+#include "position/motorOdom.hpp"
+#include "position/odomWheel.hpp"
+
+// State Machine
+#include "stateMachine/stateMachine.hpp"
+
+#include "hardware/hardware.hpp"
+#include "hardwareAbstractions/joystick/joystick.hpp"
+#include "hardwareAbstractions/joystick/robotJoystick.hpp"
+
+// Units
+#include "units/units.hpp"
+
+// Utils
+#include "utils/point.hpp"
+#include "utils/pose2d.hpp"
+#include "utils/runningAverage.hpp"
+#include "utils/splinePoint.hpp"
+#include "utils/utils.hpp"
+#include "utils/vector.hpp"
+#include "utils/polynomialExpression.hpp"
+
+#include "telemetryRadio/telemetryManager.hpp"
+
+#include "velocityProfile/trapezoidalVelocityProfile.hpp"
+
+#include "stateMachine/state/competition/auton.hpp"
+
+#include "Logger/logger.hpp"
+
+using namespace Pronounce;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -71,9 +94,6 @@ void opcontrol(void);
 #endif
 
 #ifdef __cplusplus
-/**
- * You can add C++-only headers here
- */
 //#include <iostream>
 #endif
 
